1fd2653461d1b7fc5523f73628dbb26f16166b2a
kharatyogesh <102309977+kharatyogesh@users.noreply.github.com>
2024-12-04 05:43:06 +0000

    variable code save

---
+++ basic/variables.js
@@ -0,0 +1,14 @@
+const accountId=12345;
+let accountEmail="Yogesh@gmail.com"
+var accountPass="Yogesh@123"
+accountCity="Pune"
+let accountState
+
+// accountId=2;  //cannot change constant variable
+accountEmail="hc@gmail.com"
+accountPass="Pass@123"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,accountPass,accountState]);
+
