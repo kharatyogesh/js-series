e60cb39946940a02b5e3ccda5e727e7ec738964a
kharatyogesh <102309977+kharatyogesh@users.noreply.github.com>
2024-12-04 08:50:36 +0000

    comparison save

---
+++ basic/comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+
+//when we compare two different data types
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0); //false
+// console.log(null==0); //false
+// console.log(null>=0); //true
+
+// console.log(undefined==0); //false
+// console.log(undefined>0);   //false
+// console.log(undefined<0);  //false
+
+//=== triple equal to check equality and data type also
+console.log("2"===2);
+
+
+
+
+
+
+
+
