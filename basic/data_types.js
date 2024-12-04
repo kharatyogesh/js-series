9c069ad9d58b4ec0ed2feab14888a1655bfafa70
kharatyogesh <102309977+kharatyogesh@users.noreply.github.com>
2024-12-04 06:01:36 +0000

    Data type save

---
+++ basic/data_types.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version..
+
+// alert(3+3)  // We are using NodeJs not browser so it is not used in it
+
+console.log(3+3);
+console.log("Yogesh");
+
+let name="Yogesh"
+let age=23
+let isLoggedIn=false
+let state=null
+
+//number=> range 2 to power 53
+//bigint
+//string=> "" double quote
+//boolean=> true or false
+//null=> standalone value/representation empty value
+//undefined=> value not assigned to it
+//symbol=> unique data then used symbol
+
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //Object
