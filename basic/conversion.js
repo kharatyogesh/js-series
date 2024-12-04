79176521504f2d067735f0316d4f28138a9461ad
kharatyogesh <102309977+kharatyogesh@users.noreply.github.com>
2024-12-04 06:33:58 +0000

    conversion save

--- basic/conversion.js
+++ basic/conversion.js
@@ -1,35 +1,83 @@
 // let score="35abc"
 // let score=null
 // let score=undefined
-let score=true
-console.log(typeof score);
+// let score=true
+// console.log(typeof score);
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
-let valueInNumber=Number(score)
-console.log(typeof(valueInNumber));
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber));
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
-//conversion to number
-//"33" => 33
-//"33abc"=> NaN Not a Number
-//true=> 1
-//false=>0
+// //conversion to number
+// //"33" => 33
+// //"33abc"=> NaN Not a Number
+// //true=> 1
+// //false=>0
 
 
-// let isLoggedIn=1; //ture
-// let isLoggedIn=""; //False
-let isLoggedIn="Yogesh" //True
+// // let isLoggedIn=1; //ture
+// // let isLoggedIn=""; //False
+// let isLoggedIn="Yogesh" //True
 
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-//conversion to string
-let someNumber=33 //give 33
+// //conversion to string
+// let someNumber=33 //give 33
 
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+//********************* Operations *******************
+
+let value=3;
+let negValue=-value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1="Yogesh"
+let str2=" Hello"
+
+let str3=str1 +str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2); //122
+// console.log(1+2+"2"); //32
+// console.log((3+4)*5%3);//2
+
+// console.log(+true); //gives 1
+// // console.log(true+); //gives error
+// console.log(+""); //gives 0
+
+
+// let num1,num2,num3
+
+// num1=num2=num3=2+2
+// console.log(num1);
+
+
+let gameCounter=100
+// gameCounter++;
+console.log(gameCounter++); //100  first print then increment it 100->101
+console.log(++gameCounter); //102  101 first incremenet i.e. 102 
+
+
+
+
+
+
+
 
 
