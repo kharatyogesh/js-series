353a7a8fa0b2b356255d3e69ad0d021369e82bb4
kharatyogesh <102309977+kharatyogesh@users.noreply.github.com>
2024-12-04 06:15:57 +0000

    conversions save

---
+++ basic/conversion.js
@@ -0,0 +1,35 @@
+// let score="35abc"
+// let score=null
+// let score=undefined
+let score=true
+console.log(typeof score);
+
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber);
+
+//conversion to number
+//"33" => 33
+//"33abc"=> NaN Not a Number
+//true=> 1
+//false=>0
+
+
+// let isLoggedIn=1; //ture
+// let isLoggedIn=""; //False
+let isLoggedIn="Yogesh" //True
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//conversion to string
+let someNumber=33 //give 33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+
