504be114578acb9d37650f1c69d34fcaa7946a9e
kharatyogesh <102309977+kharatyogesh@users.noreply.github.com>
2024-12-06 06:15:48 +0000

    date and time save

---
+++ basic/data_time.js
@@ -0,0 +1,20 @@
+
+// let myDate=new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate); //Object
+
+// let myCreatedDate=new Date(2023,0,23);
+// console.log(myCreatedDate.toDateString());//Mon Jan 23 2023
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate =new Date("2023-01-14");
+// console.log(myCreatedDate.toLocaleString());
+
+
+let myTimeStamp=Date.now();
+// console.log(myTimeStamp.toString());
+// console.log(Math.floor(Date.now()/1000));
+
