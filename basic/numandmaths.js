16df1a078e2465f4155a4b7a1910bc98fcabf7d3
kharatyogesh <102309977+kharatyogesh@users.noreply.github.com>
2024-12-06 05:29:17 +0000

    Math and num save

---
+++ basic/numandmaths.js
@@ -0,0 +1,47 @@
+
+//Number
+
+// const score=400
+// console.log(score);
+
+
+// const balance=new Number(100)
+// console.log(balance);
+// console.log(balance.toFixed(2));//100.00
+
+// const num=23.8966
+// console.log(num.toPrecision(3)); //23.9
+
+// const hundreds=1000000;
+// //gives commas between number
+// console.log(hundreds.toLocaleString());
+
+
+//Mathss
+
+// console.log(Math.PI);
+// console.log(Math.abs(-4)); //4 negative to positive
+// console.log(Math.round(4.6));//5
+// console.log(Math.round(4.2));//4
+
+// console.log(Math.floor(4.2));//4
+// console.log(Math.ceil(4.2));//5
+
+// console.log(Math.min(2,4,7,9));
+// console.log(Math.max(3,5,7,9));
+
+// console.log(Math.random());//change values random between 0 and 1
+
+// console.log(Math.random()*10+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)))
+
+
+
+
+
+
+
